3cb450f9b85cf257a2249cf0a20a4bd8c346a1a1
Gilles Kagarama <kagaramag@gmail.com>
2019-10-13 04:25:50 +0200

    Redesign email template

---
+++ src/helpers/mailer/header.js
@@ -0,0 +1,6 @@
+export default () => {
+  const html = `<div style="text-align:center;padding:25px;color:#878787">
+    <div style="font-weight:bold;color:#434343;font-size:30px;text-align:center">TUTELA.com</div>
+  </div>`;
+  return html;
+};
