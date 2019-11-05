eb4465115bbb21298d3942266ef6b977dbcd9a86
Googler <knowbeeinc@gmail.com>
2019-11-05 16:36:47 +0200

    fix

--- src/controllers/LegalController.js
+++ src/controllers/LegalController.js
@@ -3,30 +3,34 @@ import { Legal, User, TutorDetails } from '../queries';
 
 export default class LegalController {
   static async upload(req, res) {
-    const { id } = req.user;
-    const seniorFive = req.files && req.files[0];
-    const seniorSix = req.files && req.files[1];
-    const diploma = req.files && req.files[2];
-    const passport = req.files && req.files[3];
-    const cv = req.files && req.files[4];
-    await TutorDetails.create({
-      userId: id,
-      experience: req.body.experience,
-      language: req.body.language
-    });
-    const legalForm = await Legal.create({
-      userId: id,
-      status: 'pending',
-      seniorFive: `${seniorFive.version}/${seniorFive.public_id}.${seniorFive.format}`,
-      seniorSix: `${seniorSix.version}/${seniorSix.public_id}.${seniorSix.format}`,
-      diploma: `${diploma.version}/${diploma.public_id}.${diploma.format}`,
-      passport: `${passport.version}/${passport.public_id}.${passport.format}`,
-      cv: `${cv.version}/${cv.public_id}.${cv.format}`
-    });
+    try {
+      const { id } = req.user;
+      const seniorFive = req.files && req.files[0];
+      const seniorSix = req.files && req.files[1];
+      const diploma = req.files && req.files[2];
+      const passport = req.files && req.files[3];
+      const cv = req.files && req.files[4];
+      await TutorDetails.create({
+        userId: id,
+        experience: req.body.experience,
+        language: req.body.language
+      });
+      const legalForm = await Legal.create({
+        userId: id,
+        status: 'pending',
+        seniorFive: `${seniorFive.version}/${seniorFive.public_id}.${seniorFive.format}`,
+        seniorSix: `${seniorSix.version}/${seniorSix.public_id}.${seniorSix.format}`,
+        diploma: `${diploma.version}/${diploma.public_id}.${diploma.format}`,
+        passport: `${passport.version}/${passport.public_id}.${passport.format}`,
+        cv: `${cv.version}/${cv.public_id}.${cv.format}`
+      });
 
-    return res.status(status.CREATED).send({
-      legalForm
-    });
+      return res.status(status.CREATED).send({
+        legalForm
+      });
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   static async findBySelf(req, res) {
